b0a7e7af6693d10508de7835ed8bfe2a8923ef36
tamaron <20992019+tamaroning@users.noreply.github.com>
2024-05-17 23:23:44 +0900

    initial flipt support (#8)
    Signed-off-by: Raiki Tamura <tamaron1203@gmail.com>

---
+++ crates/flipt/src/flipt.rs
@@ -0,0 +1,174 @@
+use async_trait::async_trait;
+use flipt::evaluation::models::{EvaluationRequest, VariantEvaluationResponse};
+use open_feature::{
+    provider::{FeatureProvider, ProviderMetadata, ResolutionDetails},
+    EvaluationContext, EvaluationError, EvaluationErrorCode, EvaluationResult, StructValue, Value,
+};
+use url::Url;
+
+use crate::utils::{parse_json, translate_context, translate_error};
+
+// reexports
+pub use flipt::{ClientTokenAuthentication, JWTAuthentication, NoneAuthentication};
+
+const DEFAULT_ENTITY_ID: &str = "";
+const METADATA: &str = "flipt";
+
+pub struct Config<A>
+where
+    A: flipt::AuthenticationStrategy,
+{
+    pub url: String,
+    pub authentication_strategy: A,
+    pub timeout: u64,
+}
+
+pub struct FliptProvider {
+    metadata: ProviderMetadata,
+    client: flipt::api::FliptClient,
+    namespace: String,
+}
+
+impl FliptProvider {
+    pub fn new<A: flipt::AuthenticationStrategy>(
+        namespace: String,
+        config: Config<A>,
+    ) -> Result<Self, String> {
+        let url = match Url::parse(&config.url) {
+            Ok(url) => url,
+            Err(e) => return Err(e.to_string()),
+        };
+
+        let flipt_config = flipt::Config::new(url, config.authentication_strategy, config.timeout);
+        let client = match flipt::api::FliptClient::new(flipt_config) {
+            Ok(fpconfig) => fpconfig,
+            Err(e) => return Err(e.to_string()),
+        };
+
+        Ok(Self {
+            metadata: ProviderMetadata::new(METADATA),
+            client,
+            namespace,
+        })
+    }
+}
+
+#[async_trait]
+impl FeatureProvider for FliptProvider {
+    fn metadata(&self) -> &ProviderMetadata {
+        &self.metadata
+    }
+
+    async fn resolve_bool_value(
+        &self,
+        flag_key: &str,
+        ctx: &EvaluationContext,
+    ) -> EvaluationResult<ResolutionDetails<bool>> {
+        self.client
+            .evaluation
+            .boolean(&EvaluationRequest {
+                namespace_key: self.namespace.clone(),
+                flag_key: flag_key.into(),
+                entity_id: ctx
+                    .targeting_key
+                    .clone()
+                    .unwrap_or(DEFAULT_ENTITY_ID.to_owned()),
+                context: translate_context(ctx),
+                reference: None,
+            })
+            .await
+            .map_err(translate_error)
+            .map(|v| ResolutionDetails::new(v.enabled))
+    }
+
+    async fn resolve_int_value(
+        &self,
+        flag_key: &str,
+        ctx: &EvaluationContext,
+    ) -> EvaluationResult<ResolutionDetails<i64>> {
+        let res = variant_helper(self, flag_key, ctx).await?;
+        // parse a variant key as i64
+        res.variant_key
+            .parse::<i64>()
+            .map_err(|e| EvaluationError {
+                code: EvaluationErrorCode::General("Parse error".to_owned()),
+                message: Some(format!(
+                    "Expected a number in range of i64, but found `{}` ({:?})",
+                    res.variant_attachment, e
+                )),
+            })
+            .map(|v| ResolutionDetails::new(v))
+    }
+
+    async fn resolve_float_value(
+        &self,
+        flag_key: &str,
+        ctx: &EvaluationContext,
+    ) -> EvaluationResult<ResolutionDetails<f64>> {
+        let res = variant_helper(self, flag_key, ctx).await?;
+        // parse a variant key as f64
+        res.variant_key
+            .parse::<f64>()
+            .map_err(|e| EvaluationError {
+                code: EvaluationErrorCode::General("Parse error".to_owned()),
+                message: Some(format!(
+                    "Expected a number in range of f64, but found `{}` ({:?})",
+                    res.variant_attachment, e
+                )),
+            })
+            .map(|v| ResolutionDetails::new(v))
+    }
+
+    async fn resolve_string_value(
+        &self,
+        flag_key: &str,
+        ctx: &EvaluationContext,
+    ) -> EvaluationResult<ResolutionDetails<String>> {
+        let res = variant_helper(self, flag_key, ctx).await?;
+        // parse a variant key as i64
+        Ok(ResolutionDetails::new(res.variant_key))
+    }
+
+    async fn resolve_struct_value(
+        &self,
+        flag_key: &str,
+        ctx: &EvaluationContext,
+    ) -> Result<ResolutionDetails<StructValue>, EvaluationError> {
+        let res = variant_helper(self, flag_key, ctx).await?;
+        // parse a variant attachment as a struct value
+        let v = parse_json(&res.variant_attachment)?;
+        if let Value::Struct(sv) = v {
+            Ok(ResolutionDetails::new(sv))
+        } else {
+            Err(EvaluationError {
+                code: EvaluationErrorCode::General("Parse error".to_owned()),
+                message: Some(format!(
+                    "Expected a struct value, but found `{}`",
+                    res.variant_attachment
+                )),
+            })
+        }
+    }
+}
+
+async fn variant_helper(
+    provider: &FliptProvider,
+    flag_key: &str,
+    ctx: &EvaluationContext,
+) -> Result<VariantEvaluationResponse, EvaluationError> {
+    provider
+        .client
+        .evaluation
+        .variant(&EvaluationRequest {
+            namespace_key: provider.namespace.clone(),
+            flag_key: flag_key.into(),
+            entity_id: ctx
+                .targeting_key
+                .clone()
+                .unwrap_or(DEFAULT_ENTITY_ID.to_owned()),
+            context: translate_context(ctx),
+            reference: None,
+        })
+        .await
+        .map_err(translate_error)
+}
---
+++ crates/flipt/tests/evaluation.rs
@@ -0,0 +1,254 @@
+use std::collections::HashMap;
+
+use mockito::Server;
+use open_feature_flipt::flipt::{Config, FliptProvider, NoneAuthentication};
+use open_feature_flipt::open_feature::{provider::FeatureProvider, EvaluationContext};
+
+#[tokio::test]
+async fn test_boolean() {
+    let mut server = Server::new_async().await;
+    let mock = server
+        .mock("POST", "/evaluate/v1/boolean")
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(
+            r#"{
+        "enabled":true,
+        "reason":"DEFAULT_EVALUATION_REASON",
+        "requestId":"fb502132-66e5-45a1-a315-f1a91d4f4637",
+        "requestDurationMillis":3.070422,
+        "timestamp":"2024-05-01T10:05:06.822847492Z",
+        "flagKey":"flag_boolean"
+    }"#,
+        )
+        .create_async()
+        .await;
+
+    let config = Config {
+        url: server.url(),
+        authentication_strategy: NoneAuthentication::new(),
+        timeout: 60,
+    };
+    let ctx = EvaluationContext {
+        targeting_key: None,
+        custom_fields: HashMap::new(),
+    };
+
+    let provider = FliptProvider::new("default".to_owned(), config).unwrap();
+    let details = provider
+        .resolve_bool_value("flag_boolean", &ctx)
+        .await
+        .unwrap();
+    assert!(details.value);
+
+    // check if the mock is called once
+    mock.assert();
+}
+
+#[tokio::test]
+async fn test_boolean_unregistered() {
+    let mut server = Server::new_async().await;
+    let mock = server
+        .mock("POST", "/evaluate/v1/boolean")
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(r#"{"code":5,"message":"flag \"default/unregistered\" not found","details":[]}"#)
+        .create_async()
+        .await;
+
+    let config = Config {
+        url: server.url(),
+        authentication_strategy: NoneAuthentication::new(),
+        timeout: 60,
+    };
+    let ctx = EvaluationContext {
+        targeting_key: None,
+        custom_fields: HashMap::new(),
+    };
+
+    let provider = FliptProvider::new("default".to_owned(), config).unwrap();
+    let res = provider.resolve_bool_value("unregistered", &ctx).await;
+    assert!(res.is_err());
+
+    // check if the mock is called once
+    mock.assert();
+}
+
+#[tokio::test]
+async fn test_integer() {
+    let mut server = Server::new_async().await;
+    let mock = server
+        .mock("POST", "/evaluate/v1/variant")
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(
+            r#"{
+                "match":true,
+                "segmentKeys":["a"],
+                "reason":"MATCH_EVALUATION_REASON",
+                "variantKey":"2024",
+                "variantAttachment":"",
+                "requestId":"da64997c-92ee-4650-9585-cdcba0cb804a",
+                "requestDurationMillis":4.353005,
+                "timestamp":"2024-05-01T10:38:38.673007435Z",
+                "flagKey":"flag_integer"
+            }"#,
+        )
+        .create_async()
+        .await;
+
+    let config = Config {
+        url: server.url(),
+        authentication_strategy: NoneAuthentication::new(),
+        timeout: 60,
+    };
+    let ctx = EvaluationContext {
+        targeting_key: None,
+        custom_fields: HashMap::new(),
+    };
+
+    let provider = FliptProvider::new("default".to_owned(), config).unwrap();
+    let details = provider
+        .resolve_int_value("flag_integer", &ctx)
+        .await
+        .unwrap();
+    assert!(details.value == 2024);
+
+    // check if the mock is called once
+    mock.assert();
+}
+
+#[tokio::test]
+async fn test_float() {
+    let mut server = Server::new_async().await;
+    let mock = server
+        .mock("POST", "/evaluate/v1/variant")
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(
+            r#"{
+                "match":true,
+                "segmentKeys":["a"],
+                "reason":"MATCH_EVALUATION_REASON",
+                "variantKey":"3.1415",
+                "variantAttachment":"",
+                "requestId":"da64997c-92ee-4650-9585-cdcba0cb804a",
+                "requestDurationMillis":4.353005,
+                "timestamp":"2024-05-01T10:38:38.673007435Z",
+                "flagKey":"flag_float"
+            }"#,
+        )
+        .create_async()
+        .await;
+
+    let config = Config {
+        url: server.url(),
+        authentication_strategy: NoneAuthentication::new(),
+        timeout: 60,
+    };
+    let ctx = EvaluationContext {
+        targeting_key: None,
+        custom_fields: HashMap::new(),
+    };
+
+    let provider = FliptProvider::new("default".to_owned(), config).unwrap();
+    let details = provider
+        .resolve_float_value("flag_float", &ctx)
+        .await
+        .unwrap();
+    assert!(3.1 < details.value && details.value < 3.2);
+
+    // check if the mock is called once
+    mock.assert();
+}
+
+#[tokio::test]
+async fn test_string() {
+    let mut server = Server::new_async().await;
+    let mock = server
+        .mock("POST", "/evaluate/v1/variant")
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(
+            r#"{
+                "match":true,
+                "segmentKeys":["a"],
+                "reason":"MATCH_EVALUATION_REASON",
+                "variantKey":"Hello",
+                "variantAttachment":"",
+                "requestId":"da64997c-92ee-4650-9585-cdcba0cb804a",
+                "requestDurationMillis":4.353005,
+                "timestamp":"2024-05-01T10:38:38.673007435Z",
+                "flagKey":"flag_string"
+            }"#,
+        )
+        .create_async()
+        .await;
+
+    let config = Config {
+        url: server.url(),
+        authentication_strategy: NoneAuthentication::new(),
+        timeout: 60,
+    };
+    let ctx = EvaluationContext {
+        targeting_key: None,
+        custom_fields: HashMap::new(),
+    };
+
+    let provider = FliptProvider::new("default".to_owned(), config).unwrap();
+    let details = provider
+        .resolve_string_value("flag_string", &ctx)
+        .await
+        .unwrap();
+    assert!(details.value == "Hello");
+
+    // check if the mock is called once
+    mock.assert();
+}
+
+#[tokio::test]
+async fn test_struct() {
+    let mut server = Server::new_async().await;
+    let mock = server
+        .mock("POST", "/evaluate/v1/variant")
+        .with_status(200)
+        .with_header("content-type", "application/json")
+        .with_body(
+            r#"{
+                "match":true,
+                "segmentKeys":["a"],
+                "reason":"MATCH_EVALUATION_REASON",
+                "variantKey":"a",
+                "variantAttachment":"{\"name\":\"Miho Nishizumi\",\"message\":\"Panzer Vor!\",\"age\":17}",
+                "requestId":"da64997c-92ee-4650-9585-cdcba0cb804a",
+                "requestDurationMillis":4.353005,
+                "timestamp":"2024-05-01T10:38:38.673007435Z",
+                "flagKey":"flag_struct"
+            }"#,
+        )
+        .create_async()
+        .await;
+
+    let config = Config {
+        url: server.url(),
+        authentication_strategy: NoneAuthentication::new(),
+        timeout: 60,
+    };
+    let ctx = EvaluationContext {
+        targeting_key: None,
+        custom_fields: HashMap::new(),
+    };
+
+    let provider = FliptProvider::new("default".to_owned(), config).unwrap();
+    let details = provider
+        .resolve_struct_value("flag_struct", &ctx)
+        .await
+        .unwrap();
+    let res = details.value;
+    assert!(res.fields.get("name").unwrap().as_str().unwrap() == "Miho Nishizumi");
+    assert!(res.fields.get("message").unwrap().as_str().unwrap() == "Panzer Vor!");
+    assert!(res.fields.get("age").unwrap().as_i64().unwrap() == 17);
+
+    // check if the mock is called once
+    mock.assert();
+}
